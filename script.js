0f28e577fc094e944127e8122e15a92e976fb0d2
putcode677 <putcode701@gmail.com>
2026-05-20 23:41:41 +0300

    addEventListener()

--- script.js
+++ script.js
@@ -3,3 +3,14 @@
 
      // 2. querySelector()
     const box = document.querySelector("#box");
+
+     // 3. addEventListener()
+    document.getElementById("changeTextBtn")
+    .addEventListener("click", function(){
+
+
+         // 4. value property
+        document.getElementById("inputText").value
+      // 5. textContent
+      box.textContent = text;
+    });
